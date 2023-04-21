b2cf1061c27e463eb180333716c1f8a7defb3351
biswasbittu <biswasbittu7415@yahoo.com>
2023-04-21 12:53:20 +0530

    sign up page done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,12 +4,19 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="form-container">
-      <h2 className="form-title">Login coming sooon</h2>
+      <h2 className="form-title">Login </h2>
       <form>
         <div className="form-control">
-          <label htmlFor="">Email</label>
-          <input type="email" name="email" id="email" required placeholder="Your Email"/>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" 
+          required placeholder="Your Email"/>
         </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" 
+          required placeholder="Your Password"/>
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
       </form>
     </div>
   );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -4,9 +4,27 @@ import "./SignUp.css";
 
 const SignUp = () => {
     return (
-        <div>
-            please sign up
+        <div className="form-container">
+      <h2 className="form-title">Sign Up </h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" 
+          required placeholder="Your Email"/>
         </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" 
+          required placeholder="Your Password"/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Conform Password</label>
+          <input type="password" name="password" id="password" 
+          required placeholder="Your Password"/>
+        </div>
+        <input className="btn-submit" type="submit" value="Sign up" />
+      </form>
+    </div>
     );
 };
 
